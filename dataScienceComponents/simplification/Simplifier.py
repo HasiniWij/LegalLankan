2ad60f2a83929b473c1c99bdc66df7af6334e397
ThimaliK <72512995+ThimaliK@users.noreply.github.com>
2021-04-18 13:50:02 +0530

    Update Simplifier.py

--- dataScienceComponents/simplification/Simplifier.py
+++ dataScienceComponents/simplification/Simplifier.py
@@ -1,7 +1,4 @@
-# import pickle
-
 import spacy
-import en_core_web_sm
 import nltk
 import re
 from nltk.tokenize import word_tokenize
@@ -13,12 +10,11 @@ from pycorenlp import *
 
 nlp = StanfordCoreNLP("https://corenlp.run/")
 
+
 class Simplifier:
-
-    is_first_run=True
-    bert_model = 'bert-base-uncased'
+    is_first_run = True
+    bert_model = 'bert-large-uncased'
     model = ""
-
 
     def __init__(self):
         # bert_model = 'bert-large-uncased'
@@ -48,9 +44,7 @@ class Simplifier:
         broken_list = [str1.join(token_list1), str2.join(token_list2)]
         return broken_list
 
-
-
-#     basic Named Entity Recognition code
+    #     basic Named Entity Recognition code
     def NER_identifier(self, text):
         entity_list = []
         # nlp_ner = en_core_web_sm.load()
@@ -62,36 +56,41 @@ class Simplifier:
                 entity_list.append(word)
         return entity_list
 
-
     # BERT model to predict candidates for identified complex words
-    def get_bert_candidates(self, input_text,word,count):
-       numb_predictions_displayed = 2
-       list_candidates_bert = []
-       names_enitites = self.NER_identifier(input_text)
-#        names_enitites = []
-       lowercase_word = word.lower()
-       print(input_text)
-       if lowercase_word not in names_enitites:
-                replace_word_mask = input_text.replace(word, '[MASK]')
-                text = f'[CLS]{replace_word_mask} [SEP] {input_text} [SEP] '
-                print(text)
-                tokenized_text = self.tokenizer.tokenize(text)
-                masked_index = count
-                print("count ",count)
-                print("tokenized_text",tokenized_text)
-                indexed_tokens = self.tokenizer.convert_tokens_to_ids(tokenized_text)
-                segments_ids = [0] * len(tokenized_text)
-                tokens_tensor = torch.tensor([indexed_tokens])
-                segments_tensors = torch.tensor([segments_ids])
-                # Predict all tokens
-                with torch.no_grad():
-                    outputs = Simplifier.model(tokens_tensor, token_type_ids=segments_tensors)
-                    predictions = outputs[0][0][masked_index]
-                predicted_ids = torch.argsort(predictions, descending=True)[:numb_predictions_displayed]
-                predicted_tokens = self.tokenizer.convert_ids_to_tokens(list(predicted_ids))
-                list_candidates_bert.append((word, predicted_tokens))
-       print(list_candidates_bert)
-       return list_candidates_bert
+    def get_bert_candidates(self, input_text, word):
+
+        numb_predictions_displayed = 2
+        list_candidates_bert = []
+        names_enitites = self.NER_identifier(input_text)
+        lowercase_word = word.lower()
+
+        if lowercase_word not in names_enitites:
+            replace_word_mask = input_text.replace(word, '[MASK]')
+            text = f'[CLS]{replace_word_mask} [SEP] {input_text} [SEP] '
+            tokenized_text = self.tokenizer.tokenize(text)
+
+            index_count = 0
+            mask_index = 0
+            for token in tokenized_text:
+                if token == "[MASK]":
+                    mask_index = index_count
+                    break
+                index_count += 1
+
+            indexed_tokens = self.tokenizer.convert_tokens_to_ids(tokenized_text)
+            segments_ids = [0] * len(tokenized_text)
+            tokens_tensor = torch.tensor([indexed_tokens])
+            segments_tensors = torch.tensor([segments_ids])
+
+            # Predict all tokens
+            with torch.no_grad():
+                outputs = Simplifier.model(tokens_tensor, token_type_ids=segments_tensors)
+                predictions = outputs[0][0][mask_index]
+            predicted_ids = torch.argsort(predictions, descending=True)[:numb_predictions_displayed]
+            predicted_tokens = self.tokenizer.convert_ids_to_tokens(list(predicted_ids))
+            list_candidates_bert.append((word, predicted_tokens))
+
+        return list_candidates_bert
 
     def get_lexically_simplified_text(self, piece_list):
 
@@ -100,11 +99,11 @@ class Simplifier:
         for sentence in piece_list:
             input_no_punctuation = self.remove_punctuation(sentence)
             list_of_words = word_tokenize(input_no_punctuation)
-            count=2
+
             for word in list_of_words:
                 word = self.cleaned_word(word)
                 if zipf_frequency(word, 'en') < 4:
-                    candidates=self.get_bert_candidates(input_no_punctuation, word,count)
+                    candidates = self.get_bert_candidates(input_no_punctuation, word)
                     for candidate in candidates:
                         if candidate[0] == word:
 
@@ -115,12 +114,9 @@ class Simplifier:
                             if ps.stem(replacement) == ps.stem(word) or (not replacement.isalpha()):
                                 replacement = word
                             sentence = sentence.replace(word, replacement)
-                count+=1
+
             lexically_simplified_pieces.append(sentence)
         return lexically_simplified_pieces
-
-
-
 
     # checking whether a sentence is complete using a parse tree
     # checking whether a sentence has a conjunction
@@ -132,17 +128,15 @@ class Simplifier:
 
     def get_syntactically_simplified_text(self, input_list):
 
-
-        for i in range(0, len(input_list),2):
+        for i in range(0, len(input_list), 2):
 
             # input_piece is a string
-            broken_sentences = input_list[i],input_list[i+1]
-            input_piece=input_list[i+1]
+            broken_sentences = input_list[i], input_list[i + 1]
+            input_piece = input_list[i + 1]
 
             # if the 2 sentences broken from the conjunction form complete sentences, the splitting is successful
             text = re.sub(r"([.,?()\[\]])", "", input_piece)
             count = 0
-
 
             if (len(text.split()) > 25) and any(conjunction in text for conjunction in self.conjunction_list):
 
@@ -150,27 +144,29 @@ class Simplifier:
                     if self.word_is_a_conjunction(word):
                         token_list = self.tokenized(text)
                         sentences = self.sentences_list(token_list[:count], token_list[count + 1:])
-                        # print("Sentces",sentences)
-                        # print(type(sentences))
-                        if self.confirm_syntactic_simplification(sentences[0]) and self.confirm_syntactic_simplification(sentences[1]):
-                            broken_sentences = [input_list[i],sentences[0], sentences[1]]
+
+                        if self.confirm_syntactic_simplification(
+                                sentences[0]) and self.confirm_syntactic_simplification(sentences[1]):
+                            broken_sentences = [input_list[i], sentences[0], sentences[1]]
                             break
                     count += 1
-
-            # print("all_sentences",all_sentences)
 
         return broken_sentences
 
     def confirm_syntactic_simplification(self, text):
         sub_sub_trees = []
         parser = nlp.annotate(text, properties={"annotators": "parse", "outputFormat": "json"})
-        sent_tree = nltk.tree.ParentedTree.fromstring(parser["sentences"][0]["parse"])
-        sub_trees = list(sent_tree.subtrees())
+        try:
+            sent_tree = nltk.tree.ParentedTree.fromstring(parser["sentences"][0]["parse"])
+            sub_trees = list(sent_tree.subtrees())
 
-        if sub_trees[1].label() == "S":
-            for sub_sub_tree in sub_trees[1]:
-                sub_sub_trees.append(sub_sub_tree.label())
+            if sub_trees[1].label() == "S":
+                for sub_sub_tree in sub_trees[1]:
+                    sub_sub_trees.append(sub_sub_tree.label())
 
-            if ("VP" and "NP") in sub_sub_trees:
-                return True
+                if ("VP" and "NP") in sub_sub_trees:
+                    return True
+        except:
+            return False
+
         return False
