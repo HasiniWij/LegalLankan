1d5827ec4e0f3b6b94693e7a5ca1101fced80c3c
ThimaliK <72512995+ThimaliK@users.noreply.github.com>
2021-04-20 20:13:48 +0530

    Update UploadLeg.py

--- backend/UploadLeg.py
+++ backend/UploadLeg.py
@@ -1,70 +1,69 @@
 import pickle
 import pandas as pd
 from backend.DatabaseConnection import DatabaseConnection
+from backend.DocumentSplitter import DocumentSplitter
 from dataScienceComponents.classification.Classifier import Classifier
 
 
 class UploadLeg:
-    def __init__(self,  title, content):
-        self.title = title
-        self.content = content
 
-    def upload_data_of_piece(self, leg_index,legislation_name):
-        C = Classifier("dataScienceComponents/classification/models/svm.pickle", "dataScienceComponents"
-                                                                                    "/classification/models/tfidf"
-                                                                                    ".pickle")
+    def upload_data_of_piece(self, text):
+        splitter = DocumentSplitter()
+        legislation_name, list_dictionary_piece = splitter.split_core_legislation(text)
+        legislation_name = legislation_name.strip()
 
-        piece_category = C.get_category_of_text(self.title + self.content)
+        print("2-Doc split done")
 
-        if piece_category == "family":
+        categories = []
+
+        for piece_dictionary in list_dictionary_piece:
+            content = piece_dictionary.get("content")
+            title = piece_dictionary.get("pieceTitle")
+
+            C = Classifier("dataScienceComponents/classification/models/svm.pickle", "dataScienceComponents"
+                                                                                     "/classification/models/tfidf"
+                                                                                     ".pickle")
+
+            piece_category = C.get_category_of_text(title + content)
+            categories.append(piece_category)
+
+            count_dict = {i: categories.count(i) for i in categories}
+
+            final_category = max(count_dict, key=count_dict.get)
+
+        if final_category == "family":
             category_code = "FA"
 
-        elif piece_category == "crime":
+        elif final_category == "crime":
             category_code = "CR"
 
-        elif piece_category == "rights":
+        elif final_category == "rights":
             category_code = "RI"
 
-        elif piece_category == "employment":
+        elif final_category == "employment":
             category_code = "EM"
-
         else:
             category_code = "OT"
 
-        leg_index = leg_index.item()
-
-        self.content = self.content.replace("'", "")
-        self.content = self.content.replace("\"", "")
-        print(self.title)
-
         db = DatabaseConnection("classify-legislation")
-        sql = "INSERT INTO piece_category (pieceTitle,content,legislationIndex,categoryIndex) VALUES (%s, %s,%s,%s)"
-        val = (self.title, self.content,leg_index,category_code)
-        db.insertToDB(sql, val)
+        insert_leg_sql = "INSERT INTO legislation (legislationName, categoryIndex) VALUES (%s, %s)"
+        val = (legislation_name, category_code)
+        db.insertToDB(insert_leg_sql, val)
+        print("3-Leg name inserted")
 
-        sql = 'select pieceIndex from piece_category where pieceTitle=' + '"' + self.title + '"' + " AND content=" + '"' + self.content + '"'
+        sql = '''select l.legislationIndex from legislation l where legislationName = ''' + '"' + str(
+            legislation_name) + '"'
         sql_result = db.selectFromDB(sql)
+        leg_index = sql_result["legislationIndex"][0]
+        print("3-Leg index selected ", leg_index)
 
-        piece_index = sql_result["pieceIndex"][0]
-        print(piece_index)
+        for piece_dictionary in list_dictionary_piece:
+            content = piece_dictionary.get("content")
+            title = piece_dictionary.get("pieceTitle")
 
-#         df_path = "dataScienceComponents/extraction/models/" + piece_category + "/" + piece_category+"-df.pickle"
-#         with open(df_path, 'rb') as df:
-#             df_cat = pickle.load(df)
-#         new_df = pd.DataFrame({"pieceIndex": [piece_index], "legislationName": [legislation_name], "pieceTitle": [self.title],"content": [self.content]})
-#         df_cat.append(new_df)
-#         df.close()
+            db = DatabaseConnection("classify-legislation")
+            sql = "INSERT INTO piece ( pieceTitle,content,legislationIndex) VALUES (%s, %s,%s)"
+            val = (title, content, leg_index)
+            db.insertToDB(sql, val)
 
-#         with open(df_path, 'wb') as df:
-#             pickle.dump(df_cat, df)
-#         df.close()
-#         other_path = "dataScienceComponents/extraction/models/other/other-df.pickle"
-
-#         with open(other_path, 'rb') as df_other:
-#             other = pickle.load(df_other)
-#         other.append(new_df)
-#         df_other.close()
-
-#         with open(df_path, 'wb') as df_other:
-#             pickle.dump(df_cat, df_other)
-#         df_other.close()
+        print("4-content inserted")
