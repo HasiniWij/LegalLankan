5d925495f80807268750ceb20fdb341aad6af0e4
ThimaliK <thimali.2019303@iit.ac.lk>
2021-04-21 10:39:49 +0530

    Update Simplify.py and add comments

--- backend/DocumentSplitter.py
+++ backend/DocumentSplitter.py
@@ -1,16 +1,20 @@
-import re
+import re       # to find characters
 
 
 class DocumentSplitter:
 
+    # splits a legislation into pieces of text
     def split_core_legislation(self, full_legislation):
 
         full_leg_list = full_legislation.split("\n")
+
         total_lines = 0
 
         for line in full_leg_list:
+
             if total_lines == 0:
-                leg_name = line
+                leg_name = line     # first line is the name of the legislation
+
             total_lines += 1
 
         content_list = []
@@ -22,17 +26,18 @@ class DocumentSplitter:
         for line in full_leg_list:
 
             count += 1
-            x = re.findall("CHAPTER", line)
+            x = re.findall("CHAPTER", line)   # finds the set of characters by which each piece should be split
 
             if len(x) > 0:
 
                 title_number = line.split("CHAPTER", 1)[1].strip()
 
+                # checks for roman numerals in titles of a piece
                 if bool(re.search(r"^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$", title_number)):
                     title_count += 1
                     piece_title = str(title_count) + "-" + line
-                    piece_titles.append(piece_title)
-                    content_list.append(content)
+                    piece_titles.append(piece_title)    # title of a piece is taken separately
+                    content_list.append(content)        # content within a piece is taken separately
                     content = ""
                     continue
 
@@ -43,6 +48,7 @@ class DocumentSplitter:
                 else:
                     continue
 
+        # all pieces(titles and content) are appended to a list
         split_core_legislation = []
         i = 0
         for title in piece_titles:
@@ -50,4 +56,4 @@ class DocumentSplitter:
             split_core_legislation.append(piece)
             i += 1
 
-        return leg_name, split_core_legislation
+        return leg_name, split_core_legislation    # returned to be stored in the database
