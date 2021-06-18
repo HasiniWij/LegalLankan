b2a840fd463dd5c1e22e3b04ae5370baf3e61334
HasiniWij <61408819+HasiniWij@users.noreply.github.com>
2021-06-18 09:44:02 +0530

    Update Extractor.py

--- dataScienceComponents/extraction/Extractor.py
+++ dataScienceComponents/extraction/Extractor.py
@@ -1,34 +1,65 @@
 import jieba  # used to fot text segmentation
 import pickle  # models are stored as pickles and loaded to this file
 
+from gensim import corpora, models, similarities
+from nltk import WordNetLemmatizer, sent_tokenize
+from nltk.corpus import stopwords
+import numpy as np
+
 
 class Extractor:
 
-    def __init__(self, category):
+    def __init__(self):
 
-        common_path = "dataScienceComponents/extraction/models/" + category + "/" + category
+        common_path = "dataScienceComponents/extraction/models/"
+
         # loading the similarity matrix which stores document similarity data of all documents within the category
-        matrix_path = common_path + "_matrix.pickle"
-
+        matrix_path = common_path + "matrix.pickle"
         with open(matrix_path, 'rb') as matrix:
-            self.category_matrix = pickle.load(matrix)
+            self.matrix = pickle.load(matrix)
 
         # loading the dictionary of the category
-        dictionary_path = common_path + "_dic.pickle"
+        dictionary_path = common_path + "dic.pickle"
         with open(dictionary_path, 'rb') as cat_dictionary:
             self.dictionary = pickle.load(cat_dictionary)
 
         # loading the tfidf model of the category
-        tfidf_path = common_path + "_tfdif.pickle"
+        tfidf_path = common_path + "tfdif.pickle"
         with open(tfidf_path, 'rb') as tfidf_model:
             self.tfidf = pickle.load(tfidf_model)
 
-        # loading the piece index mapping with matrix index of the category
-        piece_index_mapping_path = common_path + "-pieceIndex-mapping.pickle"
-        with open(piece_index_mapping_path, 'rb') as mapping:
-            self.piece_index_mapping = pickle.load(mapping)
+        with open("data.pickle", 'rb') as data:
+            self.data = pickle.load(data)
+
+    def get_query_keywords(self, query):
+        query_lowered = query.lower()  # all letters are turned into lowercase
+
+        punctuation_signs = list("?:!.,;")  # punctuation signs are removed
+        for punctuation_sign in punctuation_signs:
+            query_lowered = query_lowered.replace(punctuation_sign, '')
+
+        query_lowered = query_lowered.replace("'s", "")  # apostrophes are removed
+
+        wordnet_lemmatizer = WordNetLemmatizer()  # all query words are lemmatized
+        lemmatized_query_words_list = []
+
+        query_words = query_lowered.split(" ")
+        for word in query_words:
+            lemmatized_query_words_list.append(wordnet_lemmatizer.lemmatize(word, pos="v"))
+
+        stop_words = list(stopwords.words('english'))  # all stop words within the query are removed
+        for word in lemmatized_query_words_list:
+            if word in stop_words:
+                lemmatized_query_words_list.remove(word)
+
+        # all keywords after preprocessing, turned into a string of words, and returned
+        cleaned_query = ' '.join(map(str, lemmatized_query_words_list))
+
+        return cleaned_query
 
     def get_ranked_documents(self, keywords):
+
+
         documents_with_a_rank = {}
 
         # dictionary - convert search word to vector
@@ -36,18 +67,47 @@ class Extractor:
         # doc2bow - counts the number of occurrences of a word within the corpus
         kw_vector = self.dictionary.doc2bow(jieba.lcut(keywords))
 
-        sim = self.category_matrix[self.tfidf[kw_vector]]  # checks the similarity of each document within the category
+        sim = self.matrix[self.tfidf[kw_vector]]  # checks the similarity of each document within the category
 
         for i in range(len(sim)):
             if sim[i] > 0.00:
                 documents_with_a_rank[i] = sim[i]
 
-        five_ranked_documents = sorted(documents_with_a_rank, key=documents_with_a_rank.get, reverse=True)[:5]
-        five_ranked_piece_index = []
-        for doc in five_ranked_documents:
-            for index, row in self.piece_index_mapping.iterrows():
-                if doc == index:
-                    five_ranked_piece_index.append(row['pieceIndex'])
-                    break
+        five_ranked_documents = sorted(documents_with_a_rank, key=documents_with_a_rank.get, reverse=True)[:7]
 
-        return five_ranked_piece_index
+        title_segment = {}
+        for doc in five_ranked_documents:
+            title = self.data['title'][doc]
+            sentence_list = sent_tokenize(self.data['content'][doc])
+
+            segmented_doc = []
+            for i in range(0, len(sentence_list), 4):
+                content = ""
+                if i + 4 < len(sentence_list):
+                    content = sentence_list[i] + sentence_list[i + 1] + sentence_list[i + 2] + sentence_list[i + 3]
+                    segmented_doc.append(content)
+
+
+            segmented_doc_process = [jieba.lcut(text) for text in segmented_doc]
+            dictionary_segment = corpora.Dictionary(segmented_doc_process)
+            # counts the word features of the dictionary of texts
+            feature_cnt = len(dictionary_segment.token2id)
+            # counts the number of occurrences of a word within the corpus
+            corpus = [dictionary_segment.doc2bow(text) for text in segmented_doc_process]
+            # Creates a tf-idf model
+            tfidf_segment = models.TfidfModel(corpus)
+            # Convert and index the corpus to check for similarity
+            matrix_segment = similarities.SparseMatrixSimilarity(tfidf_segment[corpus], num_features=feature_cnt)
+            kw_vector_segment = dictionary_segment.doc2bow(jieba.lcut(keywords))
+            sim_segment = matrix_segment[
+                tfidf_segment[kw_vector_segment]]  # checks the similarity of each document within the category
+
+            print("sim_segment: ", sim_segment)
+
+            index = np.where(sim_segment == np.amax(sim_segment))
+            print("index: ", index[0][0])
+
+            content_segment = segmented_doc[index[0][0]]
+            title_segment[title] = content_segment + "..."
+
+        return title_segment
